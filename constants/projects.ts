bbdcc377861499cea06f326b746e06cf431b8d9c
HimansuWebCoder <naikhimansu546@gmail.com>
2026-06-16 12:35:04 +0530

    update and fix the issue of href link and update the link

--- constants/projects.ts
+++ constants/projects.ts
@@ -1,43 +1,43 @@
 export type Project = {
-  title: string
-  image: string
-  href: string
-  description: string
-}
+  title: string;
+  image: string;
+  href: string;
+  description: string;
+};
 
 export const projects: Project[] = [
   {
-    title: 'Odisha Design Consultancy',
+    title: "Odisha Design Consultancy",
     description: `Designed and launched a digital platform for Odisha Design Consultancy to showcase 18 years of architectural excellence to national and international clients, establishing their online presence and supporting future growth.`,
-    image: '/odc.webp',
-    href: 'http://odishadesignconsultancy.com/',
+    image: "/odc.webp",
+    href: "http://odishadesignconsultancy.com/",
   },
   {
-    title: 'Pihu Makeup Studio & Salon',
+    title: "Pihu Makeup Studio & Salon",
     description:
-      'Developed a digital platform for Pihu Makeup Studio to showcase their premium beauty services and luxurious environment, enabling clients to explore and book appointments 24/7.',
-    image: '/pms.webp',
-    href: 'https://pihumakeupstudio.in/',
+      "Developed a digital platform for Pihu Makeup Studio to showcase their premium beauty services and luxurious environment, enabling clients to explore and book appointments 24/7.",
+    image: "/pms.webp",
+    href: "https://pihumakeupstudio.in/",
   },
   {
-    title: 'Mr. Kalakar Dance Studio',
+    title: "Mr. Kalakar Dance Studio",
     description:
-      'Created the first digital presence for a growing local dance academy, providing a platform to display their dance styles, programs, and help students explore opportunities from anywhere.',
-    image: '/mrkalakarsdancestudio.webp',
-    href: 'https://dance-studio-mr-kalakar.vercel.app/',
+      "Created the first digital presence for a growing local dance academy, providing a platform to display their dance styles, programs, and help students explore opportunities from anywhere.",
+    image: "/mrkalakarsdancestudio.webp",
+    href: "https://dance-studio-mr-kalakar.vercel.app/",
   },
   {
-    title: 'Sili Salon',
+    title: "Sili Salon",
     description:
-      'Designed a one-page website for Sili Salon, providing a 24/7 digital presence to showcase their beauty services and reach clients anytime, anywhere.',
-    image: '/silisalon.webp',
-    href: 'https://www.silisalon.com/',
+      "Designed a one-page website for Sili Salon, providing a 24/7 digital presence to showcase their beauty services and reach clients anytime, anywhere.",
+    image: "/silisalon.webp",
+    href: "https://www.silisalon.com/",
   },
   {
-    title: 'Achievers Institute',
+    title: "Achievers Institute",
     description:
-      'Created an all-in-one digital platform for Achievers Institute to showcase their coaching programs, teaching quality, and facilities, helping students and parents explore and connect easily.',
-    image: '/achievers-institute.webp',
-    href: 'https://achievers-institute.vercel.app/
+      "Created an all-in-one digital platform for Achievers Institute to showcase their coaching programs, teaching quality, and facilities, helping students and parents explore and connect easily.",
+    image: "/achievers-institute.webp",
+    href: "https://achievers-institute.vercel.app/",
   },
-]
+];
